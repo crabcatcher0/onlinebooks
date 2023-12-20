e0d25907118d00c2c046831fcfb8a68b99d0be10
crabcatcher0 <rautprashant103@gmail.com>
2023-12-20 18:41:10 +0545

    still remain

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to='books/covers')
+    file = models.FileField(upload_to='books/files')
+
+
+    
--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def book(request):
+    return render(request, 'index.html')
+
+def list(request):
+    data= Book.objects.all()    
+    return render(request, 'list.html', {'data':data})
